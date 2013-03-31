41dd95d90fd13b190262d73742d816f3b97931d1
Aloshi <aloshi@aloshi>
2013-03-31 14:19:34 -0500

    Fixed get_dependencies.sh to modify the Makefile for AngelScript to compile on the Raspberry Pi.

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -4,7 +4,6 @@
 #-Aloshi
 
 #Package dependencies first, because those are easy
-# +cmake for angelscript
 sudo apt-get install libsdl1.2-dev libsdl-ttf2.0-dev libsdl-image1.2-dev libboost-filesystem-dev
 
 #Install to home folder
@@ -22,6 +21,14 @@ if [ ! -e "/usr/include/angelscript.h" ] && [ ! -e "/usr/local/include/angelscri
 	wget http://www.angelcode.com/angelscript/sdk/files/angelscript_2.26.1.zip -O angelscript.zip
 	unzip angelscript.zip
 	cd sdk/angelscript/projects/gnuc
+
+	#Gotta edit the Makefile to build on the raspberry pi
+	#this is hardcoded to line 103 which is bad, but my sed skills are nonexistant
+	if lscpu | grep -q "armv6l"; then
+		echo "Adding as_callfunc_arm_gcc to AS Makefile..."
+		sed -i '103i else\n\tOBJ += \$(OBJDIR)\/as_callfunc_arm_gcc.o' makefile
+	fi
+
 	make
 	sudo make install
 	cd $HOME
